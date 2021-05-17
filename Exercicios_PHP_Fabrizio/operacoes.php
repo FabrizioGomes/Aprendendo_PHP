a13bbb848b417fc36e64d338714cb5a61700613e
Fabrizio Gomes <fabriziogomespereira@hotmail.com>
2021-05-17 16:13:40 -0300

    aprendendo php

---
+++ Exercicios_PHP_Fabrizio/operacoes.php
@@ -0,0 +1,23 @@
+<?php
+
+// $idadeDaqui10Anos = 22 + 10;
+$idade = 22;
+$idadeDaqui10Anos = $idade + 10;
+$idadeHa5anos = $idade - 5;
+
+//echo $idadeDaqui10Anos;
+//echo $idadeHa5anos;
+
+
+$soma = 2 + 2; 
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2; 
+
+$doisAoCubo =2 ** 3;
+
+$restoDaDivisao = 10 % 3; 
+
+//echo $doisAoCubo;
+//echo $restoDaDivisao;
+
---
+++ Exercicios_PHP_Fabrizio/strings.php
@@ -0,0 +1,19 @@
+<?php 
+
+$idade = 22;
+//echo "Ola Mundo! Minha idade é " . $idade;
+//echo "Ola Mundo! Minha idade é " . $idade . " anos!";
+//echo "Ola Mundo! Minha idade é $idade anos!";
+/*
+echo "Ola Mundo!\n";
+echo "\t"; 
+echo "Minha idade é $idade anos!";
+*/
+echo "Ola Mundo!" . PHP_EOL;
+echo "Minha idade é $idade anos!";
+
+echo PHP_EOL . PHP_EOL;
+
+echo "Você só pode entrar, se tiver mais de 18 anos!" . PHP_EOL;
+
+echo "Você tem $idade anos. Pode entrar!";
