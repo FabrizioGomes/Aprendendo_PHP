04d32d677ae200f38b006a5ed36b4480f5bca91a
Fabrizio Gomes <fabriziogomespereira@hotmail.com>
2021-05-21 12:24:39 -0300

    Update banco.php

--- Projeto/banco.php
+++ Projeto/banco.php
@@ -1,29 +1,34 @@
 <?php
 
-require_once 'Conta.php';
-require_once 'Titular.php';
-require_once 'Cpf.php';
+require_once 'Modelo/Conta/Conta.php';
+require_once 'Modelo/Endereco.php';
+require_once 'Modelo/Pessoa.php';
+require_once 'Modelo/Conta/Titular.php';
+require_once 'Modelo/Cpf.php';
+ 
 
-$fabrizio = new Titular(new Cpf('123.456.789-10'), 'Fabrizio Gomes');
-$primeiraConta = new Conta($fabrizio);
+//require_once 'autoload.php';
+
+use Alura\Banco\Modelo\Conta\Titular;
+use Alura\Banco\Modelo\Endereco;
+use Alura\Banco\Modelo\CPF;
+use Alura\Banco\Modelo\Conta\Conta;
+
+$endereco = new Endereco('São Paulo', 'a', 'b', '98');
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias', $endereco);
+$primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
-$primeiraConta->saca(300);
-
+$primeiraConta->saca(300); 
 
 echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia', $endereco);
+$segundaConta = new Conta($patricia);
+var_dump($segundaConta);
 
-$giovani = new Titular(new Cpf('698.549.548-10'), 'Giovani Vicente');
-$segundaConta = new Conta($giovani);
-
-echo $segundaConta->recuperaNomeTitular() . PHP_EOL;
-echo $segundaConta->recuperaCpfTitular() . PHP_EOL;
-echo $segundaConta->recuperaSaldo() . PHP_EOL;
-
-$outra = new Conta(new Titular(new Cpf('123.090.998-01'), 'Abcdefg'));
-
+$outroEndereco = new Endereco('A', 'b', 'c', '1D');
+$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $outroEndereco));
 unset($segundaConta);
-
 echo Conta::recuperaNumeroDeContas();
