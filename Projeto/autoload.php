6062590201b507320d82d000223ca69bdeeda3a0
Fabrizio Gomes <fabriziogomespereira@hotmail.com>
2021-05-21 12:58:41 -0300

    Update autoload.php

--- Projeto/autoload.php
+++ Projeto/autoload.php
@@ -1,5 +1,11 @@
 <?php
 
+require_once 'Modelo/Conta/Conta.php';
+require_once 'Modelo/Endereco.php';
+require_once 'Modelo/Pessoa.php';
+require_once 'Modelo/Conta/Titular.php';
+require_once 'Modelo/Cpf.php';
+
 spl_autoload_register(function (string $nomeCompletoDaClasse) {
     $caminhoArquivo = str_replace('Alura\\Banco', 'src', $nomeCompletoDaClasse);
     $caminhoArquivo = str_replace('\\', DIRECTORY_SEPARATOR, $caminhoArquivo);
